044c00effc9325c7ee067aa11fd03755c9d0df63
John Hoopes <john.z.hoopes@gmail.com>
2016-08-05 07:21:11 -0500

    Adding in initial selnium test case and start of wait for elements trait

---
+++ src/Traits/WaitForElements.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LaravelSeleniumDriver\Traits;
+
+
+trait WaitForElements {
+
+
+    protected $waitForTypes = ['Id', 'CssSelector', 'ClassName', 'XPath'];
+
+
+    protected function waitForElement($type, $value, $timeout)
+    {
+
+        if()
+
+
+    }
+
+
+}
